57d02b71ff2d35275093de68d3b9f3fdd83e1821
eshwardel <180693460+eshwardel@users.noreply.github.com>
2026-03-31 12:29:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import NewsItems from "./NewsItems";
+
+const NewsBoard = ({category}) => {
+ 
+   const[article,setArticles]=useState([]); 
+
+   useEffect(()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url).then(response=>response.json()).then(data=>setArticles(data.articles));  //fetch is used for to get data from server //API call → get response → convert to JSON → extract articles → store in state
+   },[category])
+  return (
+    <div>
+        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+        {article.map((news,index)=>{
+            return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+
+        })}
+    </div>
+  )
+}
+
+export default NewsBoard
