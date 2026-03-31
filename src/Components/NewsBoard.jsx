c7ab456b6943430d37b288cc1be60fa7a70dcd40
Eshwar Nandal <eshwarnandala10@gmail.com>
2026-03-31 18:17:13 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -12,10 +12,10 @@ const NewsBoard = ({category}) => {
   return (
     <div>
         <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-        {article.map((news,index)=>{
+        {article?.map((news, index) => (
             return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
 
-        })}
+        ))}
     </div>
   )
 }
